984724288e71fa7a734296f13166cbf8de70e531
iman <iman.bachatt@gmail.com>
2024-02-19 22:33:35 +0200

    modified:   contact.js

--- contact.js
+++ contact.js
@@ -1,18 +1,28 @@
 import { BASE_URL } from "./const.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var contactForm = document.getElementById("contact-form");
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the default form submission behavior
+  var submitButton = document.getElementById("submit");
+
+  // Debug: Log to console to confirm the script has loaded and button was found
+  console.log("DOMContentLoaded", submitButton);
+
+  submitButton.addEventListener("click", function (event) {
+    console.log("Button clicked"); // Debug: Log to console when the button is clicked
+
+    // Prevents the default button click behavior
+    event.preventDefault();
+    alert("Wait a minute please");
 
     // Collecting form data
     var formData = {
       name: document.querySelector("[name='name']").value,
       phone: document.querySelector("[name='number']").value,
       email: document.querySelector("[name='email']").value,
-      company: document.querySelector("[name='company']").value, // Ensure this matches the 'name' attribute in your form
-      message: document.querySelector("[name='message']").value, // Ensure this matches the 'name' attribute in your form
+      company: document.querySelector("[name='company']").value,
+      message: document.querySelector("[name='message']").value,
     };
+
+    console.log("Form data", formData); // Debug: Log the collected form data
 
     // Sending the form data using fetch
     fetch(`${BASE_URL}/contact/apply`, {
@@ -23,17 +33,24 @@ document.addEventListener("DOMContentLoaded", function () {
       body: JSON.stringify(formData),
     })
       .then(function (response) {
+        console.log("Response received"); // Debug: Log when a response is received
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
         return response.text();
       })
       .then(function (data) {
-        // Handling the response
-        console.log("Success:", data);
+        console.log("Success:", data); // Debug: Log successful data submission
         document.querySelector(".form-messege").textContent =
           "Form submitted successfully!";
+
+        // Open Calendly scheduling after form submission
+        Calendly.initPopupWidget({
+          url: "https://calendly.com/farah-b-salhab/30min?hide_gdpr_banner=1",
+        });
       })
       .catch(function (error) {
-        // Handling errors
-        console.error("Error:", error);
+        console.error("Error:", error); // Debug: Log any errors
         document.querySelector(".form-messege").textContent =
           "An error occurred!";
       });
